0d398f4f25c856b46c347e457d69cafe1458dcfa
Chris Paschall <ChrisP1108@gmail.com>
2023-02-05 13:53:47 -0700

    Forgot Password Update

--- portal_user_forgot_password.js
+++ portal_user_forgot_password.js
@@ -1,106 +1,117 @@
-    // Select Submit Button
+// Check If User Is Already Logged In To Fix Login Loop Issues With Browser Cache To Force Another Reload
     
-    const submitBtn = document.querySelector('#submit');
+if (document.cookie.includes('portal_logged_in')) {
+    window.location.reload();
+}
+
+// Select Submit Button
+
+const submitBtn = document.querySelector('#submit');
+
+// Select Error Message Field
+
+const errMsg = document.querySelector('#formErrMsg');
+
+function setErrMsg(msg) {
+    errMsg.innerHTML = msg;
+}
+
+// Button HTML Selector
+
+const btnHTML = submitBtn.querySelector('.elementor-button-text')
+
+// Capture submitBtn HTML 
+
+const btnStaticHTML = btnHTML.innerHTML;
+
+// Loading Spinner Inside Button HTML
+
+const spinnerHTML = '<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>';
+
+function setBtnHTML(html) {
+    btnHTML.innerHTML = html;
+}
+
+// Monitor For Inputs Typed To Clear Error Message If Present And Monitor For Enter Key
+
+const inputs = document.querySelectorAll('#portalLoginForm input');
     
-    // Select Error Message Field
-    
-    const msgText = document.querySelector('#formErrMsg');
-    console.log(msgText)
-    
-    function setMsg(msg) {
-        msgText.innerHTML = msg;
-    }
-    
-    // Button HTML Selector
-    
-    const btnHTML = submitBtn.querySelector('.elementor-button-text')
-    
-    // Capture submitBtn HTML 
-    
-    const btnStaticHTML = btnHTML.innerHTML;
-    
-    // Loading Spinner Inside Button HTML
-    
-    const spinnerHTML = '<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>';
-    
-    function setBtnHTML(html) {
-        btnHTML.innerHTML = html;
-    }
-    
-    // Monitor For Inputs Typed To Clear Error Message If Present And Monitor For Enter Key
-    
-    const input = document.querySelector('#portalLoginForm input');
-        
-    input.addEventListener('input', () => setMsg('')); 
-    input.addEventListener('keyup', (e) => {
+inputs.forEach(input => {
+   input.addEventListener('input', () => setErrMsg('')); 
+   input.addEventListener('keyup', (e) => {
        if (e.key === 'Enter') {
            submitForm();
        }
-    });
+   })
+});
 
-    // Makes HTTP Request On Form Submit
+// Makes HTTP Request On Form Submit
+
+function submitForm() {
+    const email = document.querySelector('#loginEmail').value;
+    const password = document.querySelector('#loginPassword').value;
     
-    function submitForm() {
-        const email = document.querySelector('#emailRecover').value;
-        
-        // Check If Email Field Is Missing
-        
-        if (!email) {
-            setMsg('Please provide your email');
-            return;
-        }
-        if (!email.includes('@')) {
-            setMsg('Please provide a valid email');
-            return;
-        }
-        
-        // Execute HTTP Req To API If Fields Properly Filled
-        
-        const domainURL = 'http://box2496.temp.domains/~foundbw0/magellanfinancial.com';
-        
-        // Loading Spinner
-        
-        setBtnHTML(spinnerHTML);
-        
-        async function submitEmailHTTP() {
-            try {
-                const res = await fetch(domainURL + '/wp-json/portal/user/forgot', {
-                   method: 'POST',
-                   headers: {
-                       'content-type': 'application/json'
-                   },
-                   body: JSON.stringify({ email })
-                });
+    // Check For Missing Fields
+    
+    if (!email && !password) {
+        setErrMsg('Please provide your email and password');
+        return;
+    }
+    if (!email) {
+        setErrMsg('Please provide your email');
+        return;
+    }
+    if (!email.includes('@')) {
+        setErrMsg('Please provide a valid email');
+        return;
+    }
+    if (!password) {
+        setErrMsg('Please provide your password');
+        return;
+    }
+    
+    // Execute HTTP Req To API If Fields Properly Filled
+    
+    const domainURL = 'http://box2496.temp.domains/~foundbw0/magellanfinancial.com';
+    
+    // Loading Spinner
+    
+    setBtnHTML(spinnerHTML);
+    
+    async function loginHTTPReq() {
+        try {
+            const res = await fetch(domainURL + '/wp-json/portal/user/login', {
+               method: 'POST',
+               headers: {
+                   'content-type': 'application/json'
+               },
+               body: JSON.stringify({ email, password})
+            });
+            
+            if (res.ok) {
                 
-                setBtnHTML(btnStaticHTML);
+                const data = await res.json();
                 
-                if (res.ok) {
-                    submitBtn.remove();
-                    document.querySelector('#emailHeading').remove();
-                    document.querySelector('#portalLoginForm').remove();
-                    setMsg('Thank you.  You will receive an email shortly with a new temporary password to log back in.');
-                    setTimeout(() => {
-                       window.location.replace("http://box2496.temp.domains/~foundbw0/magellanfinancial.com/test-portal-user-login/"); 
-                    }, 10000);
-                } else {
-                    if (res.status === 400) {
-                        setMsg('You provided an invalid email.  Please try again or contact Magellan for support.');
-                    }
-                    if (res.status >= 500) {
-                        setMsg('An error occured in submitting your email.  Please try again or contact Magellan for support.');
-                    }
-                    return;
-                }
-            } catch (err) {
+                setTimeout(() => {
+                    if (data.data.updated_password === "0") {
+                        window.location.replace("http://box2496.temp.domains/~foundbw0/magellanfinancial.com/test-portal-user-update-information/");
+                    } else window.location.replace("http://box2496.temp.domains/~foundbw0/magellanfinancial.com/test-portal-page/");
+                }, 1000);
+            } else {
                 setBtnHTML(btnStaticHTML);
-                setMsg('Connection error. Please check your internet connection and try again.');
-                console.error(err);
+                setErrMsg('Login failed. Please check your credentials and try again.  If you have forgotten your password, click the "Forgot Password?" button below.')
+                return;
             }
+        } catch (err) {
+            setBtnHTML(btnStaticHTML);
+            setErrMsg('Connection error. Please check your internet connection and try again.');
+            console.error(err);
         }
-        
-        submitEmailHTTP();
-    };
+    }
     
-    // Monitor For Button Click And Execute Login Attempt
-    
-    submitBtn.addEventListener('click', submitForm);
+    loginHTTPReq();
+};
+
+// Monitor For Button Click And Execute Login Attempt
+
+submitBtn.addEventListener('click', submitForm);
